fb2be007e4d033b6051de181a708b5c4d8c93f30
Danil Korotenko <danil.korotenko@gmail.com>
2024-05-13 13:34:51 +0300

    Properties.

--- Sources/ICDevice+CameraBrowser.h
+++ Sources/ICDevice+CameraBrowser.h
@@ -11,12 +11,14 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface ICDevice(CameraBrowserExtension)
-- (BOOL)canSyncClock;
-- (BOOL)canTakePicture;
-- (BOOL)canDeleteOneFile;
-- (BOOL)canDeleteAllFiles;
-- (BOOL)canReceiveFile;
-- (BOOL)canEject;
+
+@property(readonly) BOOL canSyncClock;
+@property(readonly) BOOL canTakePicture;
+@property(readonly) BOOL canDeleteOneFile;
+@property(readonly) BOOL canDeleteAllFiles;
+@property(readonly) BOOL canReceiveFile;
+@property(readonly) BOOL canEject;
+
 @end
 
 NS_ASSUME_NONNULL_END
